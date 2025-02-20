597e27e11437f48eca88def3f41a1095c207b04d
loic-rouzaud <loic.rouzaud@epitech.eu>
2025-02-20 16:02:35 +0100

    [ADD] - good url

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://tunedown:azerty123@db/tunedown_db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
