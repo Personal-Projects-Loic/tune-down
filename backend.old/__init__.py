989217c887aef432411359c6993d0fcfcad426d2
loic-rouzaud <loic.rouzaud@epitech.eu>
2025-02-22 21:12:47 +0100

    [DEL] - useless files + firebase

--- backend.old/__init__.py
+++
@@ -1,2 +0,0 @@
-from wallet import generate_wallet, is_valid_xrpl_wallet
-from wallet.models import WalletResponse, WalletRequest, ValidationResponse
--- backend.old/wallet/__init__.py
+++
@@ -1,2 +0,0 @@
-from .wallet import generate_wallet, is_valid_xrpl_wallet
-from .models import WalletResponse, WalletRequest, ValidationResponse
