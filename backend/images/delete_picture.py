723a56b4689bac83e105a3021e354f13233cdddf
404_Patate <71853641+Carasssiusaurat@users.noreply.github.com>
2025-03-01 15:15:18 +0100

    [UPDATE] error handler on delete picture

--- backend/images/delete_picture.py
+++ backend/images/delete_picture.py
@@ -6,30 +6,23 @@ async def delete_picture(
     filename: str,
     minio: Minio = Depends(minio_client)
 ):
+    print("delete_picture", filename)
     try:
-        if not minio.bucket_exists(buckets['nft-tests']):
-            print(f"Bucket '{buckets['nft-tests']}' does not exist.")
+        bucket_name = buckets['nft-tests']
+        if not minio.bucket_exists(bucket_name):
+            print(f"Bucket '{bucket_name}' does not exist.")
             return False
-        # Check if object exists before attempting to delete
+
         try:
-            minio.stat_object(buckets['nft-tests'], filename)
-        except Exception as err:
-            if err.code == 'NoSuchKey':
-                print(
-                    f"Object '{filename}' does not exist in bucket '{
-                        buckets['nft-tests']
-                    }'."
-                )
-                return False
-            raise  # Re-raise other errors
-        # Delete the object
-        minio.remove_object(buckets['nft-tests'], filename)
-        print(
-            f"Successfully deleted '{filename}' from bucket '{
-                buckets['nft-tests']
-            }'."
-        )
+            minio.stat_object(bucket_name, filename)
+        except Exception:
+            print(f"Object '{filename}' not found in '{bucket_name}'.")
+            return False
+
+        minio.remove_object(bucket_name, filename)
+        print(f"Successfully deleted '{filename}' from '{bucket_name}'.")
         return True
+
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
