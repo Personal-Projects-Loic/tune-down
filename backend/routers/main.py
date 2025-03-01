90d4f2f0f9b5c2de6167d803cbdd07ba61040979
404_Patate <71853641+Carasssiusaurat@users.noreply.github.com>
2025-03-01 12:29:09 +0100

    [DELETE] upload picture route

--- backend/routers/main.py
+++ backend/routers/main.py
@@ -3,10 +3,8 @@ from fastapi import APIRouter
 from routers.auth.main import router as auth_router
 from routers.user.main import router as user_router
 from routers.wallet.main import router as wallet_router
-from routers.images.main import router as images_router
 main_router = APIRouter()
 
 main_router.include_router(auth_router, prefix="/auth")
 main_router.include_router(wallet_router, prefix="/wallet")
 main_router.include_router(user_router, prefix="/user")
-main_router.include_router(images_router, prefix="/images")
