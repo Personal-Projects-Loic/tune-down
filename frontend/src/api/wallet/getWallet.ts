80c81848a93ae78366212263f349928eefef243e
404_Patate <71853641+Carasssiusaurat@users.noreply.github.com>
2025-03-02 14:41:41 +0100

    [ADD] notify to api call

--- frontend/src/api/wallet/getWallet.ts
+++ frontend/src/api/wallet/getWallet.ts
@@ -1,4 +1,5 @@
 import { Wallet } from "../../types/wallet";
+import { notify } from "../../utils/notify";
 
 export const getWallet = async (): Promise<Wallet | null> => {
   try {
@@ -10,15 +11,18 @@ export const getWallet = async (): Promise<Wallet | null> => {
 
     if (response.status === 204) {
       console.log("Aucun portefeuille trouvé pour cet utilisateur");
+      notify({ title: "Portefeuille introuvable", message: "Aucun portefeuille trouvé pour cet utilisateur", type: "warning" });
       return null
     }
 
     if (!response.ok) {
+      notify({ title: "Erreur", message: "Erreur lors de la récupération des données du portefeuille", type: "error" });
       throw new Error("Erreur lors de la récupération des données du portefeuille");
     }
 
     const data = await response.json();
     console.log("Données du portefeuille récupérées avec succès :", data);
+    notify({ title: "Portefeuille récupéré", message: "Données du portefeuille récupérées avec succès", type: "success" });
     return data;
   } catch (err) {
     console.error(err);
