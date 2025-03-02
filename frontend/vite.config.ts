17ac7e179699b3547d236b2cc0f7c01f05971599
loic-rouzaud <loic.rouzaud@epitech.eu>
2025-03-02 23:24:28 +0100

    [FIX] - cookies issue, trying to fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   server: {
     port: 3000,
     allowedHosts: ["tunedown.fr", "159.69.154.76:*"],
+    cors: {
+      origin: "http://tunedown.fr",
+      credentials: true,
+    },
+    hmr: command === "serve" ? true : false,
   },
-});
+}));
