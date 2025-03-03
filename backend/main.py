9342ea66e277bbd5641bba29852293bc498edda2
loic-rouzaud <loic.rouzaud@epitech.eu>
2025-03-03 12:57:03 +0100

    [FIX] - cors error

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,7 @@ origins = [
     "http://localhost:3000",
     "http://159.69.154.76:3000",
     "http://159.69.154.76:8000",
+    "http://tunedown.fr",
     "https://tunedown.fr",
     "https://api.tunedown.fr",
 ]
